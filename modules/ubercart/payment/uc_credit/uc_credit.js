a8cea0df2985dc033bc8bf5a2745aceec0b4a533
Yoann PETIT <zwapox@gmail.com>
2011-04-27 17:46:25 +0200

    Ajout du module ubercart MAIS pas encore instaler !

---
+++ modules/ubercart/payment/uc_credit/uc_credit.js
@@ -0,0 +1,16 @@
+
+jQuery(document).ready(
+  function () {
+    jQuery('#cc_details_title').show(0);
+    jQuery('#cc_details').hide(0);
+  }
+);
+
+/**
+ * Toggles credit card details on the order view screen.
+ */
+function toggle_card_details() {
+  jQuery('#cc_details').toggle();
+  jQuery('#cc_details_title').toggle();
+}
+
